a7320b4fbb2082c101e85de06a20563f0dd9b2d8
Yang Liu <yangliu3456@gmail.com>
2020-08-10 10:32:49 +0000

    take out extra branches on backdoor path

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# frontal
-causal inference graphs for determine causal relationships
+# Frontal
+Frontal lobe is known to process reasoning in human, hence the package name 
+frontal. This package is used to analysze causal inference graphs for 
+determining causal relationships. It's based on Judea Pearl's book about 
+Directed Acyclic Graphs. Using the book CounterFactuals and Causal Inference
+by Stephen L Mogan and Christopher Winship. 
+
+# Usage
+`python frontal/graphy.py`
+
+For generating graph picture in `graph` directory `graphy.png`. The numbered
+graphs `1.png` is each backdoor path between start and end nodes. It is used
+to check whether the backdoor criterion is satified and give a set of variables
+to condition on, so causal relationship can be determined. 
--- frontal/graph.py
+++ frontal/graph.py
@@ -1,9 +1,9 @@
-from typing import List, Tuple
-import networkx as nx
-from networkx.algorithms.dag import is_directed_acyclic_graph, ancestors
 from networkx.algorithms.traversal.edgedfs import edge_dfs
-
+from typing import List, Tuple
 import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.algorithms import dag
+from copy import deepcopy
 
 
 def draw_graph(G: nx.DiGraph,
@@ -56,18 +56,69 @@ def is_ancestor(G: nx.DiGraph,
     Returns:
         bool: whether the relationship is correct
     """
-    
-    anscestors = ancestors(G, descendent)
+
+    anscestors = dag.ancestors(G, descendent)
     if anscestor in anscestors:
         return True
     else:
         return False
 
 
+def is_end_node(graph: nx.DiGraph,
+                node: str) -> bool:
+    """Check if a node is an end node. Doesn't matter whether it's in or out
+
+    Args:
+        graph (nx.DiGraph): graph of networkx     
+        node (str): node of networkx
+
+    Returns:
+        bool: whether it's end node
+    """
+
+    if graph.out_degree(node) + graph.in_degree(node) == 1:
+        return True
+
+    return False
+
+
+def remove_extra_branches(path: nx.DiGraph,
+                          end_nodes: List[str]) -> nx.DiGraph:
+    """Remove end node that's not in the direct pathway from start to end
+
+    Args:
+        path (nx.DiGraph): path in graph format, potentially have extra branches
+        that the end nodes are not start or end
+        end_nodes (List[str]): start and end nodes
+
+    Returns:
+        nx.DiGraph: cleaned direct graph from start to end
+    """
+
+    new_path = deepcopy(path)
+    for node in path.nodes():
+        if (node not in end_nodes) \
+                and (is_end_node(graph=new_path, node=node)):
+            new_path.remove_node(node)
+
+    return new_path
+
+
 def find_all_paths(edges: Tuple,
                    nodes_count: int,
-                   cause_node: str,
-                   result_node: str) -> List:
+                   start_node: str,
+                   end_node: str) -> List:
+    """Find all path from start_node to end_node
+
+    Args:
+        edges (Tuple): deep first search edges
+        nodes_count (int): how many total nodes
+        start_node (str): start/parent node that could be the cause
+        end_node (str): end/child node that may be the result
+
+    Returns:
+        List: a list of all the path from start node to end node
+    """
     paths = []
     path = nx.DiGraph()
 
@@ -87,10 +138,12 @@ def find_all_paths(edges: Tuple,
         # find the nodes with only one nodes connected to it
         path_count = index + 1
         end_nodes = [x for x in path.nodes()
-                     if path.out_degree(x) == 0 or path.in_degree(x) == 0]
+                     if is_end_node(graph=path, node=x)]
 
-        # check if all nodes are added
-        if (cause_node in end_nodes) and (result_node in end_nodes):
+        # if end nodes is the same, then it's one version of the paths
+        if (start_node in end_nodes) and (end_node in end_nodes):
+            path = remove_extra_branches(path=path,
+                                         end_nodes=[start_node, end_node])
             draw_graph(G=path, path=f"graphs/{len(paths)+1}.png")
             paths.append(path)
             path.clear()
@@ -98,18 +151,18 @@ def find_all_paths(edges: Tuple,
     return paths
 
 
-def met_backdoor_criteria(G: nx.DiGraph,
-                          cause_node: str,
-                          result_node: str) -> bool:
+def met_backdoor_criterion(G: nx.DiGraph,
+                           start_node: str,
+                           end_node: str) -> bool:
     # edge cases
-    if not is_directed_acyclic_graph(G):
+    if not dag.is_directed_acyclic_graph(G):
         print("The graph is not a Directed Acyclic Graph!")
         return False
 
     if not is_ancestor(G=G,
-                       anscestor=cause_node,
-                       descendent=result_node):
-        print("Cause node is not the ancestor of result_node!")
+                       anscestor=start_node,
+                       descendent=end_node):
+        print("Cause node is not the ancestor of end_node!")
         return False
 
     # determine using Pearl backdoor criteria (p109)
@@ -117,22 +170,23 @@ def met_backdoor_criteria(G: nx.DiGraph,
     edges = [x for x in edge_dfs(G=G)]
     paths = find_all_paths(edges=edges,
                            nodes_count=len(G),
-                           cause_node=cause_node,
-                           result_node=result_node)
+                           start_node=start_node,
+                           end_node=end_node)
 
     return True
 
 
 def main():
-    nodes = ['C', 'D', 'O', 'Y']
+    nodes = ['A', 'C', 'D', 'O', 'Y']
     edges = [('A', 'C'), ('C', 'O'), ('C', 'D'), ('D', 'Y'), ('O', 'Y')]
 
+    # networkx use large G as graph, not lower case
     G = make_graph(nodes=nodes,
                    edges=edges)
 
-    print(met_backdoor_criteria(G=G,
-                                cause_node='D',
-                                result_node='Y'))
+    print(met_backdoor_criterion(G=G,
+                                 start_node='D',
+                                 end_node='Y'))
 
 
 if __name__ == "__main__":
